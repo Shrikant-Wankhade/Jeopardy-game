9ec8492728cdc5a549938fcf0cc308eac8376c08
Shrikant-Wankhade <wshrikantp10@gmail.com>
2022-11-21 11:21:08 +0530

    added intial data for the game

---
+++ app.js
@@ -0,0 +1,118 @@
+const jeopardycategories =[
+    {
+        genre: "WHO",
+        questions: [
+            {
+                question: "Who wrote harry potter?",
+                answers:["Harry Potter","JK Rowling"],
+                correct:"JK Rowling",
+                level:"easy"
+            },
+            {
+                question: "Who was born on Krypton?",
+                answers:["Aquaman","Superman"],
+                correct:"Superman",
+                level:"medium"
+            },
+            {
+                question: "Who designed the first car?",
+                answers:["Karl Benz","Henry Ford"],
+                correct:"Karl Benz",
+                level:"hard"
+            }
+        ]
+    },
+    {
+        genre: "WHERE",
+        questions: [
+            {
+                question: "Where is Buckingham palace?",
+                answers:["Richmond","London"],
+                correct:"London",
+                level:"easy"
+            },
+            {
+                question: "Where is colossuem?",
+                answers:["Rome","Milen"],
+                correct:"Rome",
+                level:"medium"
+            },
+            {
+                question: "Where is mount kilimanjaro?",
+                answers:["Zimbabwe","Tanzania"],
+                correct:"Tanzania",
+                level:"hard"
+            }
+        ]
+    },
+    {
+        genre: "WHEN",
+        questions: [
+            {
+                question: "When is christmas?",
+                answers:["30th DEc","25th Dec"],
+                correct:"JK Rowling",
+                level:"easy"
+            },
+            {
+                question: "When was JFK shot?",
+                answers:["1963","1961"],
+                correct:"1963",
+                level:"hard"
+            },
+            {
+                question: "When was WW2?",
+                answers:["1932","1941"],
+                correct:"1941",
+                level:"hard"
+            }
+        ]
+    },
+    {
+        genre: "WHAT",
+        questions: [
+            {
+                question: "What is the capital of India?",
+                answers:["Delhi","Mumbai"],
+                correct:"Delhi",
+                level:"easy"
+            },
+            {
+                question: "What do cats eat?",
+                answers:["Milk","banana"],
+                correct:"Milk",
+                level:"easy"
+            },
+            {
+                question: "What is kg short for?",
+                answers:["kilojoule","kilogram"],
+                correct:"kilogram",
+                level:"hard"
+            }
+        ]
+    },
+    {
+        genre: "HOW MANY",
+        questions: [
+            {
+                question: "How many players ina cricket team?",
+                answers:["9","11"],
+                correct:"11",
+                level:"easy"
+            },
+            {
+                question: "How many people in china?",
+                answers:["1.1 bil","1.4 bil"],
+                correct:"1.4 bil",
+                level:"medium"
+            },
+            {
+                question: "How many seconds in an hour?",
+                answers:["36000","3600"],
+                correct:"3600",
+                level:"hard"
+            }
+        ]
+    },
+]
+
